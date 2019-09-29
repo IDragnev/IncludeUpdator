543f246924533a71db1bb66ea2e049d2b2ce20bf
IDragnev <iddragnev@gmail.com>
2019-09-29 16:23:16 +0300

    Add tests for the ChangedFiles module

---
+++ test/changed_files_test.exs
@@ -0,0 +1,16 @@
+defmodule IncludeUpdator.ChandgeFilesTest do
+  use ExUnit.Case
+
+  alias IncludeUpdator.ChangedFiles, as: ICF
+
+  test "adding a file" do
+    filenames = sample_filenames()
+    filenames |> Enum.each(&ICF.add_filename/1)
+    assert filenames |> Enum.all?(& &1 in ICF.filenames())
+  end
+
+  defp sample_filenames do
+    1..10 |> Enum.map(&to_string(&1))
+  end
+
+end
