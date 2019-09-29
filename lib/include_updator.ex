a1fd47eac333568da22ca3cf6ad39c68271dd331
IDragnev <iddragnev@gmail.com>
2019-09-29 14:21:36 +0300

    Initial commit

---
+++ lib/include_updator.ex
@@ -0,0 +1,18 @@
+defmodule IncludeUpdator do
+  @moduledoc """
+  Documentation for IncludeUpdator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IncludeUpdator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/include_updator_test.exs
@@ -0,0 +1,8 @@
+defmodule IncludeUpdatorTest do
+  use ExUnit.Case
+  doctest IncludeUpdator
+
+  test "greets the world" do
+    assert IncludeUpdator.hello() == :world
+  end
+end
